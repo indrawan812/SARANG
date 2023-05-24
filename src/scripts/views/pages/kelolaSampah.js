4ae4b557d8e1fccfb301ee14f0b40ba54d251a33
indrawan812 <103586146+indrawan812@users.noreply.github.com>
2023-05-24 23:12:07 +0800

    Membuat halaman layanan
    Membuat halaman layanan pada website SARANG

--- src/scripts/views/pages/kelolaSampah.js
+++ src/scripts/views/pages/kelolaSampah.js
@@ -11,13 +11,13 @@ const KelolaSampah = {
           <div class="info-kelolaSampah">
               <h2>Kelola Sampah Organik</h2>
               <hr>
-              <p>Pupuk Kompos</p>
+              <p class="underscore">Pupuk Kompos</p>
               <ul>
                   <li>Pilihlah tempat yang terpisah di halaman rumah Anda untuk mengumpulkan sampah organik seperti sisa makanan, daun kering, dan potongan tanaman</li>
                   <li>Giling atau potong bahan organik tersebut menjadi potongan kecil agar lebih cepat terurai. Campurkan bahan organik tersebut dengan sedikit tanah dan air.</li>
                   <li>Biarkan kompos terurai secara alami, dan dalam beberapa bulan, Anda akan memiliki kompos yang kaya akan nutrisi untuk tanaman.</li>
               </ul>
-              <p>Pupuk Cair</p>
+              <p class="underscore">Pupuk Cair</p>
               <p>Campurkan sampah organik dengan air dalam wadah tertutup dan biarkan selama beberapa minggu. Saring campuran tersebut dan gunakan air hasil saringan sebagai pupuk cair untuk tanaman.</p>
           </div>
         `;
